b03678a9ccec89502401d014a213f6ab0744ad29
hankyang94 <hankyang94@gmail.com>
2018-05-22 13:13:54 -0400

    tuning VCA

--- VCA_RL/simple_VCA.py
+++ VCA_RL/simple_VCA.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
 
 class SimpleVoiceCoilActuator():
     def __init__(self):
@@ -24,6 +26,7 @@ class SimpleVoiceCoilActuator():
 
         return states_over_time
 
+
 u_max = 5
 f = 50.0
 total_time = 1.0/f
@@ -45,56 +48,49 @@ print len(time_array)
 print len(input_trajectory)
 print simulated_states.shape
 
-fig0 = plt.figure()
-fig0.add_subplot(211)
-plt.plot(time_array, input_trajectory)
-plt.xlabel("t")
-plt.ylabel("u[V]")
-plt.title("Original Control Policy")
-fig0.add_subplot(212)
-plt.plot(time_array, simulated_states[:, 0])
-plt.plot(time_array, states_goal, 'r')
-plt.xlabel("t")
-plt.ylabel("q[m]")
-plt.title("Original position trajectory")
-
-plt.show()
+fig0, axarray0 = plt.subplots(2, sharex=True)
+axarray0[0].plot(time_array*1000, input_trajectory)
+axarray0[0].set_ylabel("Original u[V]")
+axarray0[1].plot(time_array*1000, simulated_states[:, 0], label='Original q')
+axarray0[1].plot(time_array*1000, states_goal, 'r', label='q goal')
+axarray0[1].set_xlabel("t[ms]")
+axarray0[1].set_ylabel("q[m]")
 
 previous_state_cost = np.linalg.norm(simulated_states[:, 0] - states_goal)**2
 print "Cost: " + str(previous_state_cost)
 
+fig1, axarray1 = plt.subplots(2)
+fig2, ax2 = plt.subplots()
 mu = 0
-sigma = 1.5
-learning_rate = 1e1
+sigma = 0.1
+learning_rate = 3e0
+kappa = 0.5
+tao = 1
 iter = 0
-while (previous_state_cost > 1e-5) and (iter < 100000):
+while (previous_state_cost > 1e-6) and (iter < 1000):
     beta = np.random.normal(mu, sigma, input_trajectory.shape)
     input_trajectory_ptb = np.clip((input_trajectory + beta), -u_max, u_max)
     simulated_states = simpleVCA.simulate_states_over_time(state_initial, time_array, input_trajectory_ptb)
-    # fig.add_subplot(211)
-    # plt.plot(time_array, input_trajectory_ptb)
-    # fig.add_subplot(212)
-    # plt.plot(time_array, simulated_states[:, 0])
-    # plt.plot(time_array, states_goal, 'r')
-    # plt.show()
+
+    axarray1[0].clear()
+    axarray1[1].clear()
+    axarray1[0].set_ylabel('Perturbated u[V]')
+    axarray1[1].set_xlabel('t[ms]')
+    axarray1[1].set_ylabel('q[m]')
+    axarray1[0].plot(time_array*1000, input_trajectory_ptb)
+    axarray1[1].plot(time_array*1000, simulated_states[:, 0], label='Learned q')
+    axarray1[1].plot(time_array*1000, states_goal, 'r', label='q goal')
 
     current_state_cost = np.linalg.norm(simulated_states[:, 0] - states_goal) ** 2
-    input_trajectory -= learning_rate/((1+iter) ** (0.01)) * (current_state_cost - previous_state_cost) * beta
+    input_trajectory -= learning_rate/((tao+iter/200) ** kappa) * np.sign(current_state_cost - previous_state_cost) * beta
+    input_trajectory -= learning_rate * (current_state_cost - previous_state_cost) * beta
     input_trajectory = np.clip(input_trajectory, -u_max, u_max)
     previous_state_cost = current_state_cost
     print str(iter) + " Cost: " + str(previous_state_cost)
     iter += 1
 
-fig = plt.figure()
-fig.add_subplot(211)
-plt.plot(time_array, input_trajectory)
-plt.xlabel("t")
-plt.ylabel("u[V]")
-plt.title("Learned Control Policy")
-fig.add_subplot(212)
-plt.plot(time_array, simulated_states[:, 0])
-plt.plot(time_array, states_goal, 'r')
-plt.xlabel("t")
-plt.ylabel("q[m]")
-plt.title("Learned position trajectory")
+    ax2.scatter(iter, previous_state_cost, color='black')
+    ax2.set_yscale('log')
+    plt.pause(0.001)
+
 plt.show()
