04de3fed185a1f750d0cf484556672a80fc5ad86
hankyang94 <hankyang94@gmail.com>
2018-05-22 16:31:55 -0400

    update VCA learning

--- VCA_RL/simple_VCA_partial_learn.py
+++ VCA_RL/simple_VCA_partial_learn.py
@@ -29,11 +29,12 @@ class SimpleVoiceCoilActuator():
 
 u_max = 10
 f = 50.0
-total_time = 1.0/f
+total_time = 1.0/f/2
 dt = 0.001
 time_array = np.arange(0, total_time+dt, dt)
 state_initial = [0.0, 0.0]
 gamma = 1.0
+discount = np.power(gamma, time_array*1000/2)
 # initial guess
 input_trajectory = np.zeros_like(time_array)
 # input_trajectory[time_array < total_time/2.] = u_max
@@ -58,7 +59,7 @@ axarray0[1].set_xlabel("t[ms]")
 axarray0[1].set_ylabel("q[m]")
 axarray0[0].set_title('Original policy')
 
-error = (simulated_states[:, 0] - states_goal)
+error = discount*(simulated_states[:, 0] - states_goal)
 previous_state_cost = np.linalg.norm(error)**2
 print "Cost: " + str(previous_state_cost)
 
@@ -70,7 +71,6 @@ kappa = 0.5
 tao = 1
 iter_max = 1000
 iter = 0
-tol = 1e-6
 
 fig1, axarray1 = plt.subplots(2, sharex=True)
 axarray1[0].set_title('Learned policy')
@@ -84,9 +84,9 @@ line_g, = axarray1[1].plot(time_array*1000, states_goal, 'r', label='q goal')
 axarray1[1].legend()
 
 fig2, ax2 = plt.subplots()
-line_cost, = ax2.plot([], [], lw=4)
+line_cost, = ax2.plot([], [], lw=3)
 ax2.set_yscale('log')
-ax2.set_ylim(tol, 1e-2)
+ax2.set_ylim(1e-8, 1e-2)
 ax2.set_xlim(0, iter_max)
 ax2.set_title('Cost: Sigma = ' + str(sigma) + ', eta = ' + str(learning_rate))
 ax2.set_xlabel('Iteration')
@@ -95,7 +95,7 @@ ax2.set_ylabel('Error')
 iter_his = []
 cost_his = []
 
-while (previous_state_cost > tol) and (iter < iter_max):
+while (previous_state_cost > 1e-8) and (iter < iter_max):
     beta = np.random.normal(mu, sigma, input_trajectory.shape)
     input_trajectory_ptb = np.clip((input_trajectory + beta), -u_max, u_max)
     simulated_states = simpleVCA.simulate_states_over_time(state_initial, time_array, input_trajectory_ptb)
@@ -103,7 +103,7 @@ while (previous_state_cost > tol) and (iter < iter_max):
     line_u.set_data(time_array*1000, input_trajectory_ptb)
     line_q.set_data(time_array*1000, simulated_states[:, 0])
 
-    error = (simulated_states[:, 0] - states_goal)
+    error = discount*(simulated_states[:, 0] - states_goal)
     current_state_cost = np.linalg.norm(error) ** 2
     input_trajectory -= learning_rate/((tao+iter/500) ** kappa) * np.sign(current_state_cost - previous_state_cost) * beta
     # input_trajectory -= learning_rate * (current_state_cost - previous_state_cost) * beta
